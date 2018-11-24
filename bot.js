ee8d3c729c5fb1b05d964201a792855538f12113
TrevorWong <43641008+TrevorWong@users.noreply.github.com>
2018-11-24 17:08:39 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,20 +1,24 @@
 var Discord = require('discord.js');
 var bot = new Discord.Client();
+const fs = require('fs');
 
 bot.on('message', message => {
   var sender = message.author;
   var msg = message.content.toUpperCase();
   var prefix = '>'
 
-  //command
-if (msg === prefix + 'CC') {
-      message.channel.send( 'The Bot has connected!' )
-    }
-
-  
-  
-  
+  //Basic---Command
+  if (msg === prefix + 'CC') {
+    message.channel.send( 'The Bot has connected!' )
+  }
+  //Eco---System
+  if (!userdata[sender.id + message.guild.id])  userdata[sender.id + message.guild.id] = {}
+  if (!userdata[sender.id + message.guild.id].money) userdata[sender.id + message.guild.id].money = 100;
+  //Eco---Command
+  if (msg === prefix + 'CC') {
+    message.channel.send(userdata[sender.id + message.guild.id].money)
+  }
 });
 
-//Event
+//Bot_Token
 bot.login(process.env.bot_token);
